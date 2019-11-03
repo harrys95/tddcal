40ba42c9a006fb451ae261c979a28ae734f58508
harry <42839484+harrys95@users.noreply.github.com>
2019-11-02 22:28:58 -0700

    Add files via upload

---
+++ calculator/src/main/java/calculator/Calculator.java
@@ -0,0 +1,45 @@
+package calculator;
+
+public class Calculator {
+
+	    public int calculate(int num1, int num2, Character operation) {
+	        switch(operation) {
+	            case '+': 
+	                return add(num1, num2);
+	            case '-': 
+	                return subtract(num1, num2);
+	            case '*': 
+	                return multiply(num1, num2);
+	            case '/': 
+	                return divide(num1, num2);
+	            case '%': 
+	                return modulus(num1, num2);
+	            default:
+	                throw new IllegalArgumentException("The Operation is not valid");
+	        }
+	    }
+
+	    private int add(int num1, int num2) {
+	        return num1 + num2;
+	    }
+
+	    private int subtract(int num1, int num2) {
+	        return num1 - num2;
+	    }
+
+	    private int multiply(int num1, int num2) {
+	        return num1 * num2;
+	    }
+
+	    private int divide(int num1, int num2) {
+	        if (num2 == 0) {
+	            throw new ArithmeticException("Can not divid by zero!");
+	        }
+	        return num1 / num2;
+	    }
+
+	    private int modulus(int num1, int num2) {
+	        return num1 % num2;
+	    }
+	}
+
---
+++ calculator/src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,56 @@
+package calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	 Calculator calculatingProgram;
+
+	    @Before
+	    public void setup() {
+	        calculatingProgram = new Calculator();
+	    }
+
+	    @Test
+	    public void testAddOperation() {
+	        int result = calculatingProgram.calculate(1, 2, '+');
+	        assertTrue(result == 3);
+	    }
+
+	    @Test
+	    public void testSubtractOperation() {
+	        int result = calculatingProgram.calculate(10, 1, '-');
+	        assertTrue(result == 9);
+	    }
+
+	    @Test
+	    public void testMultiplyOperation() {
+	        int result = calculatingProgram.calculate(7, 9, '*');
+	        assertTrue(result == 63);
+	    }
+
+	    @Test
+	    public void testDivideOperation() {
+	        int result = calculatingProgram.calculate(15, 3, '/');
+	        assertTrue(result == 5);
+	    }
+
+	    @Test
+	    public void testModulusOperation() {
+	        int result = calculatingProgram.calculate(10, 3, '%');
+	        assertTrue(result == 1);
+	    }
+
+	    @Test(expected = IllegalArgumentException.class)
+	    public void testInvalidOperation() {
+	        calculatingProgram.calculate(10, 3, '0');
+	    }
+
+	    @Test(expected = ArithmeticException.class)
+	    public void textDivideByZeroException() {
+	        calculatingProgram.calculate(10, 0, '/');
+	    }
+	}
